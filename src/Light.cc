f3f534a8fe3b0431b64f4e9ac66b004a97a94a48
vonhyou <moi@rfung.dev>
2024-02-11 18:07:04 -0500

    implement setters

--- src/Light.cc
+++ src/Light.cc
@@ -1,1 +1,9 @@
 #include "Light.h"
+
+void Light::setTransform(const Matrix4f &transform) {
+  this->transform = transform;
+}
+
+void Light::setGridSize(unsigned int gridSize) { this->gridSize = gridSize; }
+
+void Light::setUseCenter(bool useCenter) { this->useCenter = useCenter; }
