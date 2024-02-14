48f00d194fea83de6626d9ad7b1002d6d3e45fdb
vonhyou <moi@rfung.dev>
2024-02-13 21:40:56 -0500

    design parser class

--- src/Parser.h
+++ src/Parser.h
@@ -1,6 +1,23 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-class Parser {};
+#include "../external/json.hpp"
+
+#include "Geometry.h"
+#include "Light.h"
+#include "Scene.h"
+
+class Parser {
+public:
+  static Geometry *parseGeometry(const nlohmann::json &);
+  static Light *parseLight(const nlohmann::json &);
+  static Scene *parseScene(const nlohmann::json &);
+
+private:
+  static PointLight *parsePointLight(const nlohmann::json &);
+  static AreaLight *parseAreaLight(const nlohmann::json &);
+  static Rectangle *parseRectangle(const nlohmann::json &);
+  static Sphere *parseSphere(const nlohmann::json &);
+};
 
 #endif // !PARSER_H_
